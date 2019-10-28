756e30b8ce73e9f451cda2ea4d077b8e4857f443
Dongkyun Rhee <dongkyun.rhee@gmail.com>
2019-10-28 13:52:14 +0900

    Create logrotate.py

---
+++ logrotate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import configparser
+from datetime import datetime
+import schedule # pip install schedule
+
+CONFIG_PATH = '/etc/oops'
+CONFIG_FILE = 'logrotate.conf'
+LOG_PATH = '/var/log/oops'
+LOG_FILE = 'logrotate.log'
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
